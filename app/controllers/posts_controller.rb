60e14aae252cc1309c741766d3ba3e5c8115f0b3
Andrew Cummings <students@themakersquare.com>
2013-10-20 16:45:28 -0500

    Add style changes to index and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.sort_by {|m| m.created_at }.reverse
   end
 
   # GET /posts/1
