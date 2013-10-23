1f2487cde83f50814b662e627d20735a3683adfb
Jesse Kevon <jessekevon@Jesses-MacBook-Pro.local>
2013-10-23 11:20:15 -0500

    Users now recieve email upon sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,12 @@
+# Similar to controllers, I want a variable to be accessible to the view, so i store it in an instance variable.
+# 
+# Mail is a method that prepares an e-mail message.
 class UserMailer < ActionMailer::Base
-  default from: "jessekevon@gmail.com"
-
   def welcome_email(user)
     @user = user
-    @url = 'http://blah.com/login'
-    mail (to: @user.email, subject: 'Welcome to Mixtape:Exchange!')
+
+    mail(to: @user.email, 
+      from: 'themixtape@gmail.com',
+      subject: 'Welcome to Mixtape:Exchange!')
   end
 end
