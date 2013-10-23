dcc8d2e31f1079313a83deac72a878a3b13695e0
Jesse Kevon <jessekevon@Jesses-MacBook-Pro.local>
2013-10-22 20:01:00 -0500

    Create user mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Defines
+  def welcome_email(user)
+    @user = user
+    @url = 'http://blah.com/login'
+    mail (to: @user.email, subject: 'Welcome to Mixtape:Exchange!')
+  end
+end
