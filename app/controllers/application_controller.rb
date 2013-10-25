3f73c650ff9d3fb5aaf16247196af390a6ee0460
Jesse Kevon <jessekevon@Jesses-MacBook-Pro.local>
2013-10-25 17:01:24 -0500

    Add style to sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,6 @@ class ApplicationController < ActionController::Base
 	  devise_parameter_sanitizer.for(:sign_up) << :name
 	  devise_parameter_sanitizer.for(:sign_up) << :profile_pic
 	end
+
+  
 end
